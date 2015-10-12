47c128c73a58c9ea265168a7c173b946f4488819
kyh671 <0__hoon@naver.com>
2015-10-12 14:27:40 +0900

    과제

---
+++ 150922.py
@@ -0,0 +1,14 @@
+print "당신이 태어난 년도를 입력하세요"
+year = int(raw_input());
+age = 2015-year+1;
+
+if (20<=age<=26):
+    print "대학생"
+elif (17<=age<20):
+    print "고등학생"
+elif (14<=age<17):
+    print "중학생"
+elif (8<=age<14):
+    print "초등학생"
+else:
+    print "학생이아닙니다."
